2d81b51b7e1561e78c7f25ddac577a9013c53535
Daniele Karoline <carvalho.danielekr@gmail.com>
2022-12-16 18:00:41 -0300

    parcial

--- DanieleK/src/main/java/carvalho/DanieleK/model/Member.java
+++ DanieleK/src/main/java/carvalho/DanieleK/model/Member.java
@@ -67,11 +67,11 @@ public class Member implements Serializable {
         this.id = id;
     }
 
-    public String getName() {
+    public String getNome() {
         return name;
     }
 
-    public void setName(String name) {
+    public void setNome(String name) {
         this.name = name;
     }
 
--- DanieleK/src/main/java/carvalho/DanieleK/model/Usuario.java
+++ DanieleK/src/main/java/carvalho/DanieleK/model/Usuario.java
@@ -1,55 +1,59 @@
 package carvalho.DanieleK.model;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.persistence.Transient;
-import javax.validation.constraints.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.Email;
-
-@Entity(name = "usuario")
+@Entity
+@Table(name = "usuario")
 public class Usuario {
-	@NotNull (message = "O nome precisa ser preenchido!")
-	private String name;
-	
-	@NotNull (message = "O e-mail precisa ser preenchido!")
-	@Email(regexp = ".+@.+\\..+")
-	private String email;
-	
-	@NotNull (message = "O telefone precisa ser preenchido!")
-	private String phone;
-	
-	@NotNull (message = "O CPF precisa ser preenchido!")
-	private String cpf;
-	
-	@NotNull (message = "A senha precisa ser preenchida!")
-	@Size(min = 6, max = 20, message = "A senha deve ter de 6 a 20 caracteres!")
-	private String pass;
 	
 	@Id
 	@GeneratedValue
+	@Column(name = "id_us")
 	private Integer id_us;
+	
+	@NotNull (message = "O nome precisa ser preenchido!")
+	@Size(min = 3, max = 30, message = "O nome deve ter de 3 a 30 caracteres!")
+	@Column(name = "nome")
+	private String nome;
+	
+	@NotNull (message = "O e-mail precisa ser preenchido!")
+	@Email(regexp = ".+@.+\\..+")
+	@Column(name = "email")
+	private String email;
+	
+	
+	@NotNull (message = "A senha precisa ser preenchida!")
+	@Size(min = 6, max = 20, message = "A senha deve ter de 6 a 20 caracteres!")
+	@Column(name = "pass")
+	private String pass;
+	
+	@NotNull (message = "O CPF precisa ser preenchido!")
+	@Column(name = "cpf")
+	private String cpf;
+	
 	
 	@Transient
 	private String aux;
 	
 	public String getName() {
-		return name;
+		return nome;
 	}
-	public void setName(String name) {
-		this.name = name;
+	public void setName(String nome) {
+		this.nome = nome;
 	}
 	public String getEmail() {
 		return email;
 	}
 	public void setEmail(String email) {
 		this.email = email;
-	}
-	public String getPhone() {
-		return phone;
-	}
-	public void setPhone(String phone) {
-		this.phone = phone;
 	}
 	public String getCpf() {
 		return cpf;
